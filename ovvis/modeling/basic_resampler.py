0c9c05555798384e32b9aa6c7454550cf26a708f
sennnnn <cyanlaser@stu.pku.edu.cn>
2023-10-25 21:21:50 +0800

    remove trash files.

--- ovvis/modeling/basic_resampler.py
+++
@@ -1,9 +0,0 @@
-import einops
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-from .transformer_decoder.video_mask2former_transformer_decoder import SelfAttentionLayer, CrossAttentionLayer, FFNLayer, MLP
-
-
-
